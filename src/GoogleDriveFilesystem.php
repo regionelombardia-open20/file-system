2841f5dfd1738172fefec62e79fff9cf8e45213b
Aria S.p.A <info@ariaspa.it>
2023-10-24 01:32:46 +0200

    first commit

---
+++ src/GoogleDriveFilesystem.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace open20\file\system;
+
+use creocoder\flysystem\Filesystem;
+use Google_Client;
+use Google_Service_Drive;
+use Hypweb\Flysystem\GoogleDrive\GoogleDriveAdapter;
+use League\Flysystem\AdapterInterface;
+use yii\base\InvalidConfigException;
+
+/**
+ * GoogleDriveFilesystem class
+ *
+ * */
+class GoogleDriveFilesystem extends Filesystem {
+
+    public $clientId;
+    public $clientSecret;
+    public $refreshToken;
+    public $rootFolderId;
+    public $driveId;
+
+    /**
+     * @inheritdoc
+     */
+    public function init() {
+        if ($this->clientId === null) {
+            throw new InvalidConfigException('The "clientId" property must be set.');
+        }
+
+        if ($this->clientSecret === null) {
+            throw new InvalidConfigException('The "clientSecret" property must be set.');
+        }
+
+        if ($this->refreshToken === null) {
+            throw new InvalidConfigException('The "refreshToken" property must be set.');
+        }
+
+        parent::init();
+    }
+
+    /**
+     * @return AdapterInterface
+     */
+    protected function prepareAdapter() {
+        $client = new Google_Client();
+        $client->setClientId($this->clientId);
+        $client->setClientSecret($this->clientSecret);
+        $client->refreshToken($this->refreshToken);
+        $service = new Google_Service_Drive($client);
+
+        $options = [];
+        if (isset($this->driveId)) {
+            $options['teamDriveId'] = $this->driveId;
+        }
+
+        return new GoogleDriveAdapter($service, $this->rootFolderId, $options);
+    }
+
+}
